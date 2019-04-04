17dbd76a8cab41b7ed711a3efa53693d93712358
Teck9i9eteen <43444429+Teck9i9eteen@users.noreply.github.com>
2019-04-04 18:16:04 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,89 @@
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === 'Earth rotates on its axis') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === '1* per day') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+ $(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === '70%') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === ' dry air rising,expanding, and cooling') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === 'Thermometer') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === 'Saturn') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === '4.6 years') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === 'A Tsunami') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === 'Wind') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
+
+$(".Check").click(function() {
+    var inputValue = $(".Type your answer").val();
+    if(inputValue  === 'Rotation of earth on its axis') {
+        alert('you are correct');
+    } else {
+        alert('you are incorrect, try again');
+    }
+});
